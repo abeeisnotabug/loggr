893a2e58512d7625cfb2237cc37b81395911b9f0
Maximilian Bee <richard.maximilian.bee@fu-jena.de>
2020-12-08 01:11:14 +0100

    Use match.call().

--- R/global.R
+++ R/global.R
@@ -1,13 +1,19 @@
 initialize_progress <- function(...) {
   cl_pid <- Sys.getpid()
   cat(sprintf("#!id:%i", cl_pid), sep = "\n")
-  
+
   iterator_variables <- list(...)
-  
+
   cat(
     sprintf(
       "#!vars;%s",
-      paste(sprintf("%s=%s", names(iterator_variables), paste(iterator_variables)), collapse = ",")
+      paste(
+        sprintf(
+          "%s=%s",
+          names(iterator_variables),
+          paste(iterator_variables)),
+        collapse = ","
+      )
     ),
     sep = "\n"
   )
@@ -15,15 +21,14 @@ initialize_progress <- function(...) {
 
 log_progress <- function(..., cluster_pid = "cl_pid") {
   cl_pid <- ifelse(is.character(cluster_pid), get(cluster_pid), cluster_pid)
-  
-  iterator_names <- as.character(sys.call()[-1])
-  iterator_values <- c(...)
-  
+
+  call_args <- as.list(match.call(expand.dots = FALSE))
+
   cat(
     sprintf(
       "#!iter;%i-%i,%s",
       cl_pid, Sys.getpid(),
-      paste(sprintf("%s=%s", iterator_names, iterator_values), collapse = ",")),
+      paste(sprintf("%s=%s", names(call_args$...), call_args$...), collapse = ",")),
     sep = "\n"
   )
 }
