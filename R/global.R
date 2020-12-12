d2f70b4df273a52bc027437b7ad568dc6ad3c819
Maximilian Bee <richard.maximilian.bee@fu-jena.de>
2020-12-12 11:12:02 +0100

    Rewrite functions to log to a one-line file instead of appending.

--- R/global.R
+++ R/global.R
@@ -1,17 +1,32 @@
 initialize_progress <- function(...) {
-  command_args <- commandArgs()
-  if ("--interactive" %in% command_args) {
+  if ("--interactive" %in% commandArgs()) {
     stop("This command must be run in an R script that is either sourced or run via 'Rscript' in the terminal.")
   }
 
-  cl_pid <- Sys.getpid()
-  cat(sprintf("#!id:%i", cl_pid), sep = "\n")
-
   iterator_variables <- list(...)
+
+  if (any("" %in% names(iterator_variables))) {
+    stop("All arguments must be named with format iterator_name = iterator_values.")
+  }
+
+  script_pid <- Sys.getpid()
 
   cat(
     sprintf(
-      "#!vars;%s",
+      "#!id;%s;%i",
+      format(Sys.time(), "%y.%m.%d-%H.%M.%OS6"),
+      script_pid
+    ),
+    sep = "\n"
+  )
+
+  cat(sprintf("#!cmd;%s", paste(list(commandArgs()))),  sep = "\n")
+
+  cat(
+    sprintf(
+      "#!vars;%s;%i;%s",
+      format(Sys.time(), "%y.%m.%d-%H.%M.%OS6"),
+      script_pid,
       paste(
         sprintf(
           "%s=%s",
@@ -22,36 +37,148 @@ initialize_progress <- function(...) {
     ),
     sep = "\n"
   )
-}
 
-log_progress <- function(..., cluster_pid = "cl_pid") {
-  cl_pid <- ifelse(is.character(cluster_pid), get(cluster_pid), cluster_pid)
-
-  call_args <- as.list(match.call(expand.dots = FALSE))
-
-  cat(
-    sprintf(
-      "#!iter;%i-%i,%s",
-      cl_pid, Sys.getpid(),
-      paste(sprintf("%s=%s", names(call_args$...), call_args$...), collapse = ",")),
-    sep = "\n"
+  list(
+    script_pid = script_pid,
+    outfile = commandArgs(trailingOnly = TRUE)
   )
 }
 
-run_and_log <- function(script) {
-  if (!is.character(script)) {
-    stop("'script' must be a character string with the path to a .R file relative to
-         the current working directory.")
+log_progress <- function(..., loggr_object) {
+  call_args <- as.list(match.call(expand.dots = FALSE))
+  iterators <- list(...)
+
+  write(
+    sprintf(
+      "#!iter;%s;%i-%i;%s",
+      format(Sys.time(), "%y.%m.%d-%H.%M.%OS6"),
+      loggr_object$script_pid, Sys.getpid(),
+      if (is.null(names(call_args$...))) {
+        paste(
+          sprintf(
+            "%s=%s",
+            call_args$...,
+            ifelse(
+              sapply(iterators, is.numeric) | sapply(iterators, is.logical),
+              sprintf("%s", iterators),
+              ifelse(
+                sapply(iterators, is.character),
+                sprintf("'%s'", iterators),
+                sprintf("'%s'", sapply(iterators, capture.output))
+              )
+            )
+          ),
+          collapse = ","
+        )
+      } else {
+        paste(
+          sprintf(
+            "%s=%s",
+            append(
+              as.list(names(call_args$...)[names(call_args$...) != ""]),
+              call_args$...[names(call_args$...) == ""]
+            ),
+            ifelse(
+              sapply(iterators, is.numeric) | sapply(iterators, is.logical),
+              sprintf("%s", iterators),
+              ifelse(
+                sapply(iterators, is.character),
+                sprintf("'%s'", iterators),
+                sprintf("'%s'", sapply(iterators, capture.output))
+              )
+            )
+          ),
+          collapse = ","
+        )
+      }
+    ),
+    file = paste0("p", loggr_object$script_pid, "w", Sys.getpid(), ".log")
+  )
+}
+
+run_and_log <- function(simu_name, ..., append = FALSE) {
+  log_folder <- "/data/sim/_shared/simu_logs"
+
+  ##### Exception handling #####
+  if (missing(simu_name)) {
+    stop("A simulation name must be supplied.")
   }
 
-  if (!file.exists(script)) {
+  call_args <- as.list(match.call(expand.dots = FALSE))
+  scripts <- call_args$...
+
+  if (any(!sapply(scripts, is.character))) {
+    stop(
+      sprintf(
+"%s not a character string.
+The ... must be paths to .R files relative to the current working directory.",
+        paste(scripts[!sapply(scripts, is.character)], collapse = ", ")
+      )
+    )
+  }
+
+  if (any(!sapply(scripts, file.exists))) {
     cat(dir(), sep = "\n")
-    stop(sprintf("%s not found. Above are the contents of your current working directory.", script))
+    stop(
+      sprintf(
+        "%s not found. Above are the contents of your current working directory.",
+        paste(scripts[!sapply(scripts, file.exists)], collapse = ", ")
+      )
+    )
   }
 
-  if (!grepl(".R", script)) {
-    if(!askYesNo("'script' should be a .R file, continue anyway?")) {
-      return(NULL)
+  if (any(!sapply(scripts, endsWith, ".R"))) {
+    for (script in scripts[!sapply(scripts, endsWith, ".R")]) {
+      if (!askYesNo(sprintf("%s should be a .R file, continue anyway?", script))) {
+        return(NULL)
+      }
     }
   }
+
+  if (!file.exists(log_folder)) {
+    dir.create(log_folder)
+  }
+
+  if (!file.exists(file.path(log_folder, simu_name))) {
+    dir.create(file.path(log_folder, simu_name))
+  } else if (!append) {
+    overwrite <- askYesNo(
+      sprintf("The directory %s already exists. Overwrite the current directory?
+(Chosing 'No' will append to the existing files)", simu_name)
+    )
+
+    if (is.na(overwrite)) {
+      return(NULL)
+    } else if (overwrite) {
+      unlink(file.path(log_folder, simu_name), recursive = TRUE)
+
+      dir.create(file.path(log_folder, simu_name))
+    }
+  }
+
+  sapply(
+    scripts,
+    function(script) {
+      call_time <- format(Sys.time(), "%y.%m.%d-%H.%M.%OS6")
+
+      system(
+        sprintf(
+          "nohup Rscript --vanilla %s %s > %s 2> %s & echo $!",
+          script,
+          file.path(log_folder, simu_name, paste(basename(script), "cluster.log", collapse = "-")),
+          sprintf(
+            "%s-%s.out",
+            file.path(log_folder, simu_name, basename(script)),
+            call_time
+          ),
+          sprintf(
+            "%s-%s.err",
+            file.path(log_folder, simu_name, basename(script)),
+            call_time
+          )
+        ),
+        intern = TRUE
+      )
+    }
+  )
 }
---
+++ testfolder/testscript1.R
@@ -0,0 +1,16 @@
+loggr_object <- loggr::initialize_progress(prior = TRUE, type = c("u", "v"), i = 1:3, j = 1:20)
+
+library(doParallel)
+
+cl <- makeCluster(2, outfile = loggr_object$outfile)
+registerDoParallel(cl)
+
+foreach(type = c("u", "v")) %:%
+  foreach(i = 1:3) %:%
+    foreach(j = 1:20) %dopar% {
+      loggr::log_progress(prior = TRUE, type, i, j, loggr_object = loggr_object)
+
+      Sys.sleep(10)
+    }
+
+stopCluster(cl)
