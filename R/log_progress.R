5323e94bf6818eae102a7df4fd422086f2403041
Maximilian Bee <richard.maximilian.bee@fu-jena.de>
2021-01-25 08:42:57 +0100

    Rename stuff.

--- R/log_progress.R
+++ R/log_progress.R
@@ -6,18 +6,17 @@ log_progress <- function(..., loggr_object, expr) {
 
   worker_id <- Sys.getpid()
   it_count <- loggr_object$iterator$currentElem()
-
   start_time <- get_time()
 
-  log_file_names <- make_logg_file_names(loggr_object)
+  logg_file_names <- make_logg_file_names(loggr_object)
 
   count <- !loggr_object$count_explicitly
 
   logg_this_condition <- function(c) {
-    logg_condition(c = c, get_time(), loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, log_file_names$err)
+    logg_condition(c = c, get_time(), loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, logg_file_names$err)
   }
 
-  logg_iteration("start", start_time, loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, log_file_names$out)
+  logg_iteration("start", start_time, loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, logg_file_names$out)
 
   result <- try(withCallingHandlers(
     eval(substitute(expr, env = globalenv())),
@@ -41,7 +40,7 @@ log_progress <- function(..., loggr_object, expr) {
     it_count <- loggr_object$iterator$nextElem()
   }
 
-  logg_iteration("end", start_time, loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, log_file_names$out)
+  logg_iteration("end", start_time, loggr_object$parent_id, worker_id, it_count, raw_list, raw_values, logg_file_names$out)
 
   if (class(result) == "try-error") {
     stop(attr(result, "condition"))
